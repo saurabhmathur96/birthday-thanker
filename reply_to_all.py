5efc21bd14ae5ce1562b94ed42e966e4fc4228aa
Saurabh Mathur <saurabhmathur96@gmail.com>
2015-01-03 16:42:45 +0530

    Update reply_to_all.py

--- reply_to_all.py
+++ reply_to_all.py
@@ -1,37 +1,41 @@
+"""
+This script replies to the first LIMIT posts on your wall
+"""
+
 import requests
 import json
-import urllib
 
-'''
-This script replies to the first LIMIT posts on your wall
-'''
 
 TOKEN = '' #Insert token here
-
 LIMIT = '10' #Number of posts to be replied to
+    
 
 def comment_on_post(post, message):
+    """ makes a POST request to facebok that comments on a post """
     url = 'https://graph.facebook.com/v2.2/%s/comments' % post['id']
     parameters = {'access_token': TOKEN, 'message': message}
     s = requests.post(url, data = parameters)
     return s
 
-def comment_on_posts(posts):
-    """Comments on all posts"""
+def get_first_name(post):
+    """ returns first name of poster from post dict """
+    return post['from']['name'] 
+
+def main():
+    """ main function """
+    parameters = {'access_token': TOKEN, 'limit': LIMIT}
+    r = requests.get(
+        'https://graph.facebook.com/v2.2/me/feed', 
+        params=parameters
+    )
+    result = json.loads(r.text)
+    posts = result['data']
+
     for post in posts:
-        url = 'https://graph.facebook.com/%s/comments' % post['post_id']
-        message = 'Commenting through the Graph API'
-        parameters = {'access_token': TOKEN, 'message': message}
-        s = requests.post(url, data = parameters)
-
-
-parameters = {'access_token': TOKEN, 'limit': LIMIT}
-r = requests.get('https://graph.facebook.com/v2.2/me/feed', params=parameters)
-result = json.loads(r.text)
-posts = result['data']
-
-for post in posts:
-    name = get_first_name(post)
-    message = 'Thank You, '+ name
-    r = comment_on_post(post, message)
-    print r #status of post request
+        name = get_first_name(post)
+        message = 'Thank You, '+ name
+        r = comment_on_post(post, message)
+        print r #status of post request
+        
+if __name__ == '__main__':
+    main()
